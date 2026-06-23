689559be226e6048d45af6d4bc0d188bc951cc96
Bingchen Yin <azang0307@163.com>
2026-06-22 21:45:14 -0500

    Updated portfolio homepage

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ links.forEach(link => {
     link.classList.add("active");
   });
 });
+// Career Page //
+const careerTabs = document.querySelectorAll(".career-tab");
+const careerContents = document.querySelectorAll(".career-content");
+
+careerTabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    careerTabs.forEach(item => item.classList.remove("active"));
+    careerContents.forEach(item => item.classList.remove("active"));
+
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
+  });
+});
+// Projects Page //
+const projectTabs = document.querySelectorAll(".project-tab");
+const projectContents = document.querySelectorAll(".project-content");
+
+projectTabs.forEach(tab => {
+  tab.addEventListener("click", () => {
+    projectTabs.forEach(item => item.classList.remove("active"));
+    projectContents.forEach(item => item.classList.remove("active"));
+
+    tab.classList.add("active");
+    document.getElementById(tab.dataset.tab).classList.add("active");
+  });
+});
